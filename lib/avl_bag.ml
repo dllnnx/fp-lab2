caaef6dc8e62041626a4acc00719ce026d22ae45
dllnnx <alenadenisova528@gmail.com>
2025-12-07 22:27:37 +0300

    formatting

--- lib/avl_bag.ml
+++ lib/avl_bag.ml
@@ -1,7 +1,7 @@
 type 'a node = {
-  value : 'a;           
-  count : int;          
-  height : int;         
+  value : 'a;
+  count : int;
+  height : int;
   left : 'a node option;
   right : 'a node option;
 }
@@ -9,85 +9,71 @@ type 'a node = {
 type 'a t = 'a node option
 
 let empty = None
-
-let is_empty = function
-  | None -> true
-  | Some _ -> false
-
-let height_of_node = function
-  | None -> 0
-  | Some node -> node.height
+let is_empty = function None -> true | Some _ -> false
+let height_of_node = function None -> 0 | Some node -> node.height
 
 let calculate_height left right =
   1 + max (height_of_node left) (height_of_node right)
 
 let make_node value count left right =
-  {
-    value = value;
-    count = count;
-    height = calculate_height left right;
-    left = left;
-    right = right;
-  }
+  { value; count; height = calculate_height left right; left; right }
 
-let balance_factor node =
-  height_of_node node.left - height_of_node node.right
+let balance_factor node = height_of_node node.left - height_of_node node.right
 
 (* повороты *)
 let rotate_right = function
   | Some { value; count; left = Some left_node; right; _ } ->
-    Some (make_node left_node.value left_node.count 
-          left_node.left 
-          (Some (make_node value count left_node.right right)))
+      Some
+        (make_node left_node.value left_node.count left_node.left
+           (Some (make_node value count left_node.right right)))
   | node -> node
 
 let rotate_left = function
   | Some { value; count; left; right = Some right_node; _ } ->
-    Some (make_node right_node.value right_node.count
-          (Some (make_node value count left right_node.left))
-          right_node.right)
+      Some
+        (make_node right_node.value right_node.count
+           (Some (make_node value count left right_node.left))
+           right_node.right)
   | node -> node
 
 let balance node =
   match node with
   | None -> None
   | Some n ->
-    let bf = balance_factor n in
-    if bf > 1 then
-      (* левый дисбаланс *)
-      match n.left with
-      | Some left_node when balance_factor left_node < 0 ->
-        (* левое поддерево тяжелее и его правое поддерево тяжелее *)
-        let new_left = rotate_left n.left in
-        rotate_right (Some { n with left = new_left })
-      | _ ->
-        (* левое поддерево тяжелее и его левое поддерево тоже тяжелее *)
-        rotate_right node
-    else if bf < -1 then
-      (* правый дисбаланс *)
-      match n.right with
-      | Some right_node when balance_factor right_node > 0 ->
-        (* правое поддерево тяжелее и его левое поддерево тяжелее *)
-        let new_right = rotate_right n.right in
-        rotate_left (Some { n with right = new_right })
-      | _ ->
-        (* правое-правое тяжелые *)
-        rotate_left node
-    else
-      Some { n with height = calculate_height n.left n.right }
+      let bf = balance_factor n in
+      if bf > 1 then
+        (* левый дисбаланс *)
+        match n.left with
+        | Some left_node when balance_factor left_node < 0 ->
+            (* левое поддерево тяжелее и его правое поддерево тяжелее *)
+            let new_left = rotate_left n.left in
+            rotate_right (Some { n with left = new_left })
+        | _ ->
+            (* левое поддерево тяжелее и его левое поддерево тоже тяжелее *)
+            rotate_right node
+      else if bf < -1 then
+        (* правый дисбаланс *)
+        match n.right with
+        | Some right_node when balance_factor right_node > 0 ->
+            (* правое поддерево тяжелее и его левое поддерево тяжелее *)
+            let new_right = rotate_right n.right in
+            rotate_left (Some { n with right = new_right })
+        | _ ->
+            (* правое-правое тяжелые *)
+            rotate_left node
+      else Some { n with height = calculate_height n.left n.right }
 
 let rec insert_rec value = function
   | None -> Some (make_node value 1 None None)
   | Some node ->
-    let cmp = compare value node.value in
-    if cmp = 0 then
-      Some { node with count = node.count + 1 }
-    else if cmp < 0 then
-      let new_left = insert_rec value node.left in
-      balance (Some { node with left = new_left })
-    else
-      let new_right = insert_rec value node.right in
-      balance (Some { node with right = new_right })
+      let cmp = compare value node.value in
+      if cmp = 0 then Some { node with count = node.count + 1 }
+      else if cmp < 0 then
+        let new_left = insert_rec value node.left in
+        balance (Some { node with left = new_left })
+      else
+        let new_right = insert_rec value node.right in
+        balance (Some { node with right = new_right })
 
 let insert value bag = insert_rec value bag
 
@@ -100,81 +86,72 @@ let rec remove_min = function
   | None -> None
   | Some { left = None; right; _ } -> right
   | Some node ->
-    let new_left = remove_min node.left in
-    balance (Some { node with left = new_left })
+      let new_left = remove_min node.left in
+      balance (Some { node with left = new_left })
 
 let rec remove_rec value = function
   | None -> None
-  | Some node ->
-    let cmp = compare value node.value in
-    if cmp < 0 then
-      (* удаляем из левого поддерева *)
-      let new_left = remove_rec value node.left in
-      balance (Some { node with left = new_left })
-    else if cmp > 0 then
-      (* удаляем из правого поддерева *)
-      let new_right = remove_rec value node.right in
-      balance (Some { node with right = new_right })
-    else
-      if node.count > 1 then
-        Some { node with count = node.count - 1 }
+  | Some node -> (
+      let cmp = compare value node.value in
+      if cmp < 0 then
+        (* удаляем из левого поддерева *)
+        let new_left = remove_rec value node.left in
+        balance (Some { node with left = new_left })
+      else if cmp > 0 then
+        (* удаляем из правого поддерева *)
+        let new_right = remove_rec value node.right in
+        balance (Some { node with right = new_right })
+      else if node.count > 1 then Some { node with count = node.count - 1 }
       else
-        match node.left, node.right with
+        match (node.left, node.right) with
         | None, None -> None
         | Some _, None -> node.left
         | None, Some _ -> node.right
         | Some _, Some _ ->
-          (* два ребенка: заменяем на минимальный из правого поддерева *)
-          let successor = find_min node.right in
-          let new_right = remove_min node.right in
-          balance (Some { node with value = successor; right = new_right })
+            (* два ребенка: заменяем на минимальный из правого поддерева *)
+            let successor = find_min node.right in
+            let new_right = remove_min node.right in
+            balance (Some { node with value = successor; right = new_right }))
 
 let remove value bag = remove_rec value bag
 
 let rec fold_left func acc = function
   | None -> acc
   | Some node ->
-    let acc_left = fold_left func acc node.left in
-    let rec fold_count f a value count =
-      if count <= 0 then a
-      else fold_count f (f a value) value (count - 1)
-    in
-    let acc_node = fold_count func acc_left node.value node.count in
-    fold_left func acc_node node.right
+      let acc_left = fold_left func acc node.left in
+      let rec fold_count f a value count =
+        if count <= 0 then a else fold_count f (f a value) value (count - 1)
+      in
+      let acc_node = fold_count func acc_left node.value node.count in
+      fold_left func acc_node node.right
 
 let rec fold_right func bag acc =
   match bag with
   | None -> acc
   | Some node ->
-    let acc_right = fold_right func node.right acc in
-    let rec fold_count f value count a =
-      if count <= 0 then a
-      else fold_count f value (count - 1) (f value a)
-    in
-    let acc_node = fold_count func node.value node.count acc_right in
-    fold_right func node.left acc_node
+      let acc_right = fold_right func node.right acc in
+      let rec fold_count f value count a =
+        if count <= 0 then a else fold_count f value (count - 1) (f value a)
+      in
+      let acc_node = fold_count func node.value node.count acc_right in
+      fold_right func node.left acc_node
 
 let filter predicate bag =
-  fold_left (fun acc value -> 
-    if predicate value then insert value acc else acc
-  ) empty bag
+  fold_left
+    (fun acc value -> if predicate value then insert value acc else acc)
+    empty bag
 
 let map func bag =
   fold_left (fun acc value -> insert (func value) acc) empty bag
 
-let union bag1 bag2 =
-  fold_left (fun acc value -> insert value acc) bag1 bag2
-
-let remove_all value bag =
-  filter (fun x -> x <> value) bag
-
+let union bag1 bag2 = fold_left (fun acc value -> insert value acc) bag1 bag2
+let remove_all value bag = filter (fun x -> x <> value) bag
 
 let rec equals bag1 bag2 =
-  match bag1, bag2 with
+  match (bag1, bag2) with
   | None, None -> true
   | None, Some _ | Some _, None -> false
   | Some node1, Some node2 ->
-    node1.value = node2.value &&
-    node1.count = node2.count &&
-    equals node1.left node2.left &&
-    equals node1.right node2.right
+      node1.value = node2.value && node1.count = node2.count
+      && equals node1.left node2.left
+      && equals node1.right node2.right
--- test/test_fp_lab2.ml
+++ test/test_fp_lab2.ml
@@ -1,8 +1,7 @@
 open Fp_lab2.Avl_bag
 
 (* вспомогательные функции *)
-let to_list bag =
-  List.rev (fold_left (fun acc x -> x :: acc) [] bag)
+let to_list bag = List.rev (fold_left (fun acc x -> x :: acc) [] bag)
 
 let count_occurrences value bag =
   fold_left (fun acc x -> if x = value then acc + 1 else acc) 0 bag
@@ -17,18 +16,18 @@ let test_empty () =
 let test_insert_single () =
   let bag = insert 5 empty in
   Alcotest.(check bool) "bag with element is not empty" false (is_empty bag);
-  Alcotest.(check int_list) "single element bag" [5] (to_list bag);
+  Alcotest.(check int_list) "single element bag" [ 5 ] (to_list bag);
   Alcotest.(check int) "count single element" 1 (count_occurrences 5 bag)
 
 let test_insert_multiple_same () =
   let bag = empty |> insert 5 |> insert 5 |> insert 5 in
-  Alcotest.(check int_list) "multiple same elements" [5; 5; 5] (to_list bag);
+  Alcotest.(check int_list) "multiple same elements" [ 5; 5; 5 ] (to_list bag);
   Alcotest.(check int) "count multiple same" 3 (count_occurrences 5 bag)
 
 let test_insert_different () =
   let bag = empty |> insert 3 |> insert 1 |> insert 5 |> insert 2 |> insert 4 in
   let sorted = List.sort compare (to_list bag) in
-  Alcotest.(check int_list) "different elements sorted" [1; 2; 3; 4; 5] sorted
+  Alcotest.(check int_list) "different elements sorted" [ 1; 2; 3; 4; 5 ] sorted
 
 (* remove тесты*)
 let test_remove_single () =
@@ -40,24 +39,26 @@ let test_remove_multiple () =
   let bag = empty |> insert 5 |> insert 5 |> insert 5 in
   let bag' = remove 5 bag in
   Alcotest.(check int) "count after single remove" 2 (count_occurrences 5 bag');
-  Alcotest.(check int_list) "remaining elements" [5; 5] (to_list bag')
+  Alcotest.(check int_list) "remaining elements" [ 5; 5 ] (to_list bag')
 
 let test_remove_nonexistent () =
   let bag = empty |> insert 1 |> insert 2 |> insert 3 in
   let bag' = remove 4 bag in
-  Alcotest.(check int_list) "unchanged after remove nonexistent" (to_list bag) (to_list bag')
+  Alcotest.(check int_list)
+    "unchanged after remove nonexistent" (to_list bag) (to_list bag')
 
 let test_remove_all () =
   let bag = empty |> insert 5 |> insert 5 |> insert 5 |> insert 3 in
   let bag' = remove_all 5 bag in
-  Alcotest.(check int) "no occurrences after remove_all" 0 (count_occurrences 5 bag');
+  Alcotest.(check int)
+    "no occurrences after remove_all" 0 (count_occurrences 5 bag');
   Alcotest.(check int) "other elements preserved" 1 (count_occurrences 3 bag');
-  Alcotest.(check int_list) "remaining elements" [3] (to_list bag')
+  Alcotest.(check int_list) "remaining elements" [ 3 ] (to_list bag')
 
 (* fold тесты *)
 let test_fold_left () =
   let bag = empty |> insert 1 |> insert 2 |> insert 3 |> insert 2 in
-  let sum = fold_left (+) 0 bag in
+  let sum = fold_left ( + ) 0 bag in
   Alcotest.(check int) "fold_left sum" 8 sum;
   let count = fold_left (fun acc _ -> acc + 1) 0 bag in
   Alcotest.(check int) "fold_left count" 4 count
@@ -70,16 +71,19 @@ let test_fold_right () =
 
 (* filter тесты *)
 let test_filter () =
-  let bag = empty |> insert 1 |> insert 2 |> insert 3 |> insert 4 |> insert 5 |> insert 6 in
+  let bag =
+    empty |> insert 1 |> insert 2 |> insert 3 |> insert 4 |> insert 5
+    |> insert 6
+  in
   let evens = filter (fun x -> x mod 2 = 0) bag in
   let sorted = List.sort compare (to_list evens) in
-  Alcotest.(check int_list) "filter evens" [2; 4; 6] sorted
+  Alcotest.(check int_list) "filter evens" [ 2; 4; 6 ] sorted
 
 let test_filter_with_duplicates () =
   let bag = empty |> insert 2 |> insert 2 |> insert 3 |> insert 4 |> insert 4 in
   let evens = filter (fun x -> x mod 2 = 0) bag in
   let sorted = List.sort compare (to_list evens) in
-  Alcotest.(check int_list) "filter with duplicates" [2; 2; 4; 4] sorted;
+  Alcotest.(check int_list) "filter with duplicates" [ 2; 2; 4; 4 ] sorted;
   Alcotest.(check int) "count filtered duplicates" 2 (count_occurrences 2 evens)
 
 (* map тесты *)
@@ -87,84 +91,90 @@ let test_map () =
   let bag = empty |> insert 1 |> insert 2 |> insert 3 in
   let doubled = map (fun x -> x * 2) bag in
   let sorted = List.sort compare (to_list doubled) in
-  Alcotest.(check int_list) "map double" [2; 4; 6] sorted
+  Alcotest.(check int_list) "map double" [ 2; 4; 6 ] sorted
 
 let test_map_with_duplicates () =
   let bag = empty |> insert 1 |> insert 2 |> insert 3 |> insert 2 in
   let doubled = map (fun x -> x * 2) bag in
   let sorted = List.sort compare (to_list doubled) in
-  Alcotest.(check int_list) "map with duplicates" [2; 4; 4; 6] sorted;
+  Alcotest.(check int_list) "map with duplicates" [ 2; 4; 4; 6 ] sorted;
   Alcotest.(check int) "count mapped duplicates" 2 (count_occurrences 4 doubled)
 
 (* property-based тесты *)
 module PBT = struct
   open QCheck
 
-  let bag_gen = Gen.(list_size (int_bound 10) (int_bound 20) |> 
-    map (List.fold_left (fun acc x -> insert x acc) empty))
+  let bag_gen =
+    Gen.(
+      list_size (int_bound 10) (int_bound 20)
+      |> map (List.fold_left (fun acc x -> insert x acc) empty))
 
   let bag_arb = make bag_gen
   let pair_arb = make Gen.(pair bag_gen bag_gen)
   let triple_arb = make Gen.(triple bag_gen bag_gen bag_gen)
 
-  let equal a b = 
-    let to_list bag = List.sort compare (fold_left (fun acc x -> x::acc) [] bag) in
+  let equal a b =
+    let to_list bag =
+      List.sort compare (fold_left (fun acc x -> x :: acc) [] bag)
+    in
     to_list a = to_list b
 
-  let qcheck_tests = [
-    Test.make ~name:"left identity" bag_arb 
-      (fun a -> equal a (union empty a));
-    
-    Test.make ~name:"right identity" bag_arb 
-      (fun a -> equal a (union a empty));
-    
-    Test.make ~name:"associativity" triple_arb 
-      (fun (a,b,c) -> equal (union (union a b) c) (union a (union b c)));
-    
-    Test.make ~name:"commutativity" pair_arb 
-      (fun (a,b) -> equal (union a b) (union b a));
-  ]
+  let qcheck_tests =
+    [
+      Test.make ~name:"left identity" bag_arb (fun a -> equal a (union empty a));
+      Test.make ~name:"right identity" bag_arb (fun a ->
+          equal a (union a empty));
+      Test.make ~name:"associativity" triple_arb (fun (a, b, c) ->
+          equal (union (union a b) c) (union a (union b c)));
+      Test.make ~name:"commutativity" pair_arb (fun (a, b) ->
+          equal (union a b) (union b a));
+    ]
 
-  let property_tests = 
-    List.map QCheck_alcotest.to_alcotest qcheck_tests
+  let property_tests = List.map QCheck_alcotest.to_alcotest qcheck_tests
 end
 
 (* все тесты *)
-let basic_tests = [
-  "empty", `Quick, test_empty;
-  "insert_single", `Quick, test_insert_single;
-  "insert_multiple_same", `Quick, test_insert_multiple_same;
-  "insert_different", `Quick, test_insert_different;
-]
+let basic_tests =
+  [
+    ("empty", `Quick, test_empty);
+    ("insert_single", `Quick, test_insert_single);
+    ("insert_multiple_same", `Quick, test_insert_multiple_same);
+    ("insert_different", `Quick, test_insert_different);
+  ]
 
-let remove_tests = [
-  "remove_single", `Quick, test_remove_single;
-  "remove_multiple", `Quick, test_remove_multiple;
-  "remove_nonexistent", `Quick, test_remove_nonexistent;
-  "remove_all", `Quick, test_remove_all;
-]
+let remove_tests =
+  [
+    ("remove_single", `Quick, test_remove_single);
+    ("remove_multiple", `Quick, test_remove_multiple);
+    ("remove_nonexistent", `Quick, test_remove_nonexistent);
+    ("remove_all", `Quick, test_remove_all);
+  ]
 
-let fold_tests = [
-  "fold_left", `Quick, test_fold_left;
-  "fold_right", `Quick, test_fold_right;
-]
+let fold_tests =
+  [
+    ("fold_left", `Quick, test_fold_left);
+    ("fold_right", `Quick, test_fold_right);
+  ]
 
-let filter_tests = [
-  "filter", `Quick, test_filter;
-  "filter_with_duplicates", `Quick, test_filter_with_duplicates;
-]
+let filter_tests =
+  [
+    ("filter", `Quick, test_filter);
+    ("filter_with_duplicates", `Quick, test_filter_with_duplicates);
+  ]
 
-let map_tests = [
-  "map", `Quick, test_map;
-  "map_with_duplicates", `Quick, test_map_with_duplicates;
-]
+let map_tests =
+  [
+    ("map", `Quick, test_map);
+    ("map_with_duplicates", `Quick, test_map_with_duplicates);
+  ]
 
 let () =
-  Alcotest.run "avl-bag" [
-    "Basic operations", basic_tests;
-    "Remove operations", remove_tests;
-    "Fold operations", fold_tests;
-    "Filter operations", filter_tests;
-    "Map operations", map_tests;
-    "Property-based tests", PBT.property_tests;
-  ]
+  Alcotest.run "avl-bag"
+    [
+      ("Basic operations", basic_tests);
+      ("Remove operations", remove_tests);
+      ("Fold operations", fold_tests);
+      ("Filter operations", filter_tests);
+      ("Map operations", map_tests);
+      ("Property-based tests", PBT.property_tests);
+    ]
